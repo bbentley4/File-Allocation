03203629ca2afe30bbed32218a08c46f8f81e044
bbentley4 <brennafierro@gmail.com>
2024-03-09 12:19:29 -0800

    adding error checking

--- Lab-2-FAT/FATRW.c
+++ Lab-2-FAT/FATRW.c
@@ -17,27 +17,61 @@ int CalcTotalSectors (void* disk_ptr)
 
 int CalculateDataSectors (int total_sectors)
 {
-    //total_sectors = T //data_sectors = D
+    // T = total sectors & D = data sectors & S = FAT sectors
+    // T = D + S
+    // S = (D+1)/512
     // T = D + ((D+1)/512)
     // T - D = (D+1)/512
     // T*512 - D*512 = D+1
     // T*512-1 = 513D
     // ((T*512)-1)/513 = D
     int data_sectors = ((total_sectors*512)-1)/513;
-    printf("DS: %i\nBKS: %i", data_sectors, total_sectors-data_sectors);
     return data_sectors;
 }
 
-
 int main(int argc, char **argv)
 {
-    // if (argc != 4 && argc != 6)
-    // {
-    //     printf("Usage: ");
-    // }
+    //Variable declaration 
+    void* disk_ptr;
+    int total_sectors, data_sectors, FAT_sectors;
+    int im_ex; // 0 = import, 1 = export
+    // Error check argument count
+    if (argc != 4 && argc != 6)
+    {
+        printf("Usage: FATRW diskfile import input-file\nFATRW diskfile export starting-block output-file");
+        return -1;
+    }
+    
+    // Attach/open disk & error check
+    disk_ptr = jdisk_attach(argv[1]);
+    if (disk_ptr == NULL)
+    {
+        perror("Could not attach diskfile.\n");
+        return -1;
+    } 
+    
+    // Check second argument 
+    if (strcmp(argv[2], "import") == 0)
+    {
+        im_ex = 0;
+    }
+    else if (strcmp(argv[2], "export") == 0)
+    {
+        im_ex = 1;
+    }
+    else 
+    {
+        printf("Usage: FATRW diskfile import input-file\nFATRW diskfile export starting-block output-file");
+        return -1;
+    }
 
-    void* disk_ptr = jdisk_attach(argv[1]);
-    int total_sectors =  CalcTotalSectors(disk_ptr);
-    int data_sectors =  CalculateDataSectors(total_sectors);
+    //Check third argument 
+    if (im_ex == 0)
+    {
+        
+    }
+    total_sectors =  CalcTotalSectors(disk_ptr);
+    data_sectors =  CalculateDataSectors(total_sectors);
+    FAT_sectors = total_sectors - data_sectors;
     return 0;
 }
